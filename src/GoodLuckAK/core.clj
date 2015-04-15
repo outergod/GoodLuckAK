dd5f1dedc7888f7f0afc21263e37ca64647948d3
Antti Heinonen <antti.k.heinonen@here.com>
2015-04-15 11:15:32 +0200

    initial

--- src/GoodLuckAK/core.clj
+++ src/GoodLuckAK/core.clj
@@ -4,4 +4,5 @@
 (defn -main
   [& args]
   (println "It was a great pleasure to work with you. Good luck and all the best in the future!")
-  (println "Mircea & Zsolt"))
+  (println "Mircea & Zsolt")
+  (println "Antti Heinonen"))
