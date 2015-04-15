caf9ff4e03983b98d0c54625e3e1d7946e5680bb
Stefan Hübner <sthuebner@googlemail.com>
2015-04-15 15:03:42 +0200

    REAL PROGRAMMERS USE emacs! https://xkcd.com/378/

--- src/GoodLuckAK/core.clj
+++ src/GoodLuckAK/core.clj
@@ -13,7 +13,8 @@
     (println " - Zeno .. thank you for showing me nyan mode and helping me to set up my beloved Visual Studio shortcuts in Emacs.")
     (println " - count .. Thanks for all the effort & heart put in for Linux & OSS at HERE!")
     (println " - Lukas - I used vim for this. Who would ever use emacs?")
-    (println " - Stefan. I know I challenged your patience a lot ;)")
+    (println " - Stefan. I know I challenged your patience a lot ;)\n"
+             "           REAL PROGRAMMERS USE emacs! https://xkcd.com/378/")
     (println " - Elke - May your new adventures bring you lots of fun and excitement!")
     (println " - Tillmann")
     (println " - Matthias")
