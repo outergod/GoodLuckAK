6df1c955817c44e668002ce3b903c52879c4cbe0
Luigi Massa-Gallerano <luigi.massa-gallerano@here.com>
2015-04-15 11:29:19 +0200

    Adding my name

--- src/GoodLuckAK/core.clj
+++ src/GoodLuckAK/core.clj
@@ -5,4 +5,5 @@
   [& args]
   (println "It was a great pleasure to work with you. Good luck and all the best in the future!")
   (println " - Mircea & Zsolt")
-  (println " - Enda"))
+  (println " - Enda")
+  (println " - Luigi"))
